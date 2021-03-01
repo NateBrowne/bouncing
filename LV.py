d4522b86fa7a77c0de819b3d80400295fd89947b
NateBrowne <73693166+NateBrowne@users.noreply.github.com>
2021-03-01 16:58:35 +0000

    Experiment with limit cycle plot

--- LV.py
+++ LV.py
@@ -7,7 +7,7 @@ from ODE_Utils import *
 def dvdt(t, vect):
 
     a = 1
-    b = 0.5
+    b = 0.26
     d = 0.1
 
     x = vect[0]
@@ -20,16 +20,25 @@ def dvdt(t, vect):
 @logger.catch
 def main():
 
-    tl, vl = solve_to(dvdt, 0, 150, [.1, .1], 0.001, method='RK4')
-    vl = np.transpose(vl)
-    plt.plot(tl, vl[0], label='Prey')
-    plt.plot(tl, vl[1], label='Predator')
+    tl, vl = solve_to(dvdt, 0, 400, [.3, .3], 0.001, method='RK4')
+    vl = np.transpose(vl[1])
 
-    plt.ylabel('Population')
-    plt.xlabel('Time')
+    ##### PLOT system wrt time
+    # plt.plot(tl, vl[0], label='Prey')
+    # plt.plot(tl, vl[1], label='Predator')
+    #
+    # plt.ylabel('Population')
+    # plt.xlabel('Time')
+    #
+    # plt.title('Lotka-Volterra, b = 0.5')
+    # plt.legend()
 
-    plt.title('Lotka-Volterra, b = 0.5')
-    plt.legend()
+    ##### PLOT orbit
+    plt.plot(vl[0], vl[1], label='Orbit')
+    plt.xlabel('Predator')
+    plt.ylabel('Prey')
+
+    plt.title('Periodic orbit b = 0.1')
     plt.show()
 
 if __name__ == '__main__':
